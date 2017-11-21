b66709d3b3b4b69b1a3a907006b8b84e123a7cc5
Kieren Evans <kevans@inviqa.com>
2017-11-21 13:54:32 +0000

    Clean up unneeded Magento2 files (under NGINX)

--- lib/rake-tasks-magento2/rake_task.rb
+++ lib/rake-tasks-magento2/rake_task.rb
@@ -9,4 +9,15 @@ namespace :magento2 do
     services_from_args(services: %w[web]).exec('root', "container magento #{args.join(' ')}")
     exit $CHILD_STATUS.to_i
   end
+
+  task :clean_magento_files do
+    STDOUT.puts "==> Cleaning Magento2 files"
+    %w[.htaccess .htaccess.sample .php_cs .travis.yml .user.ini CHANGELOG.md CONTRIBUTING.md COPYING.txt
+       Gruntfile.js.sample index.php ISSUE_TEMPLATE.md LICENSE.txt LICENSE_AFL.txt nginx.conf.sample
+       package.json.sample php.ini.sample pub/.htaccess pub/.user.ini .php_cs.dist grunt-config.json.sample
+       LICENSE_EE.txt PULL_REQUEST_TEMPLATE.md README_EE.md bin/.htaccess phpserver].each do |file|
+      FileUtils.rm_rf(file)
+    end
+    STDOUT.puts "==> Cleaned Magento2 files"
+  end
 end
